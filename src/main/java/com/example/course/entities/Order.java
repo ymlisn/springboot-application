2fe2b56c8a240ef9e7cb35452a02c5e73906566d
Yann Lins <ymlins@pop-os.localdomain>
2020-05-16 11:56:21 -0300

    Payment one to one

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -31,6 +31,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
@@ -70,6 +73,14 @@ public class Order implements Serializable {
         if(orderStatus!=null){
         this.orderStatus = orderStatus.getCode();
     }}
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 
     public void setClient(User client) {
         this.client = client;
